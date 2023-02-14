b1bdf78d9a19aa592100e7fcefa0fdb08cf7defb
deepabarish <deepabarish@gmail.com>
2023-02-14 21:42:09 +1100

    Added userdefined variable

--- variable.sh
+++ variable.sh
@@ -5,6 +5,18 @@
 #System variable
 
 
-echo $BASH
+echo The bash shell I am using $BASH
 echo $HOME
 echo $PWD
+
+
+
+#User Defined variable
+#variable name isn't start with number
+
+
+name=Deepa
+echo My name is $name
+a=10
+b=67
+echo The value for a is $a
