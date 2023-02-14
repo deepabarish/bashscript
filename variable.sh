51c0b934031e652ab46c15f8a7b5ab499d6490d2
deepabarish <deepabarish@gmail.com>
2023-02-14 23:37:29 +1100

    entering the input from the user

--- variable.sh
+++ variable.sh
@@ -20,3 +20,18 @@ echo My name is $name
 a=10
 b=67
 echo The value for a is $a
+
+
+#taking input from user use read command
+
+
+echo Enter the name
+read name
+echo Entered name : $name
+
+#multiple name entering
+
+
+echo Enter the names
+read name1 name2 name3
+echo Entered names : $name1 , $name2 , $name3
