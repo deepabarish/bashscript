17ad8e1ba1a97c0f2cb08b4d1a5809f5c81818db
deepabarish <deepabarish@gmail.com>
2023-02-18 02:24:41 +1100

    added more file handling

--- file.sh
+++ file.sh
@@ -4,12 +4,49 @@
 
 #! /bin/bash
 
-echo "Enter the file name"
+#'\c' is using same line
+#'-e' flag check the file is existing or not
+
+
+echo -e "Enter the file name : \c"
 read file_name
+
+if [ -e $file_name ]
+then
+	echo "$file_name found"
+else
+	echo "$file_name not found"
+fi
+
+
+# '-f' flag used for checking file exist and it is a regular file or not
 
 if [ -f $file_name ]
 then
 	echo "$file_name found"
 else
 	echo "$file_name not found"
+fi
+
+
+# '-s' check the file is empty or not
+
+if [ -s $file_name ]
+then
+	echo "$file_name isnt empty"
+else
+	echo "$file_name is empty"
+fi
+
+
+#'-d' flag used for checking the directory is present or noti
+
+
+echo -e "Enter the directory name : "
+read dir
+if [ -d $dir ]
+then
+	echo "$dir found"
+else	
+	echo "$dir not found"
 fi
