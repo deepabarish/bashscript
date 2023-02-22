05ced7eca659ba1e6c9273f1b63c4901a5c08b2c
deepabarish <deepabarish@gmail.com>
2023-02-22 13:24:53 +1100

    Added more form of AND operator

--- logicaland.sh
+++ logicaland.sh
@@ -6,7 +6,31 @@
 echo "Enter the number : "
 read num
 
+#1.Usage of AND
+
+
 if [ $num -gt 18 ] && [ $num -lt 30 ]
+then
+	echo "$num is valid"
+else
+	echo "$num isn't valid"
+fi
+
+#2.Usage of AND
+
+
+if [ $num -gt 18 -a $num -lt 30 ]
+then
+	echo "$num is valid"
+else
+	echo "$num isn't valid"
+fi
+
+
+#3.Usage of AND
+
+
+if [[ $num -gt 18 && $num -lt 30 ]]
 then
 	echo "$num is valid"
 else
