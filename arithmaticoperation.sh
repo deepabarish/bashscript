06aebc6bb4705cb4a9d1f0901b35109810190a7e
deepabarish <deepabarish@gmail.com>
2023-02-23 05:25:44 +1100

    Added expr usage

--- arithmaticoperation.sh
+++ arithmaticoperation.sh
@@ -4,12 +4,21 @@ echo "Enter the 1st number : "
 read num1
 echo "Enter the 2nd number : "
 read num2
-
+:'
 t=$(( num1 + num2 ))
 p=$(( num1 * num2 ))
 d=$(( num1 / num2 ))
 s=$(( num1 - num2 ))
-m=$(( num1 % num2 ))
+m=$(( num1 % num2 ))'
+
+
+#Using 'expr' command
+
+t=$(expr $num1 + $num2 )
+p=$(expr $num1 \* $num2 )
+d=$(expr $num1 / $num2 )
+s=$(expr $num1 - $num2 )
+m=$(expr $num1 % $num2 )
 echo sum = $t
 echo multiplication = $p
 echo division = $d
