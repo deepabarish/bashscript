8b5f468da0c766dc9b20a356373a72a45e4a1062
deepabarish <deepabarish@gmail.com>
2023-02-25 09:26:36 +1100

    Case ststement explanation

---
+++ caseexplanation.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Case Statement Syntax
+: '
+case expression in 
+	pattern1)
+		statements ;;
+	pattern2 )
+		statements;;
+	...
+esac'
+
+vehicle=$1
+
+case $vehicle in
+	"car" )
+		echo " Rent for car is 100 doller " ;;
+	"van" )
+		echo "Rent for van is 80 doller " ;;
+	"bicycle" )
+		echo "Rent for bicycle is 5 doller " ;;
+	"scoter" )
+		echo "Rent for scotter is 20 doller " ;;
+	* )
+		echo "Unknown vehicle " ;;
+esac
+
