7671de8e9b0ba4b14fccf05a797ef645fd993c4f
deepabarish <deepabarish@gmail.com>
2023-03-28 05:48:22 +1100

    demo of bash shell

--- Array.sh
+++ Array.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
 os=("linux" "windows" "mac" )
+
+#print all the element of the array
+
+
 echo ${os[@]}
+
 echo ${os[1]}
 echo ${!os[@]}
 echo ${#os[@]}
@@ -10,3 +15,8 @@ echo ${os[@]}
 unset os[3]
 echo ${#os[@]}
 echo ${os[@]}
+
+# Associate array
+
+declare -A arr3=( [Honda]=civic [BMW]=7series [Mercedese]=Gclass )
+echo ${arr3[BMW]}
---
+++ demo.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+declare -i num=50
+
+declare -i result=num+50
+
+echo $result
